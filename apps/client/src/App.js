39172514788f39afa75d3c3ef1c4896a540f4bbe
Adi <luffy1120adi@gmail.com>
2026-07-08 21:31:27 +0530

    Clean up scaffold artifacts

--- apps/client/src/App.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-export default function App() {
-    return (_jsxs("main", { className: "app-shell", children: [_jsx("h1", { children: "NexDrop" }), _jsx("p", { children: "Peer-to-peer file sharing over WebRTC Data Channels." })] }));
-}
