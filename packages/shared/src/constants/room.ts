35b55505fc3d2385f13326518383ec305a0decd7
Adi <luffy1120adi@gmail.com>
2026-07-09 14:36:46 +0530

    feat(shared): export shared constants

---
+++ packages/shared/src/constants/room.ts
@@ -0,0 +1,9 @@
+export const ROOM_ID_LENGTH = 8;
+
+export const MAX_ROOM_SIZE = 2;
+
+export const ROOM_EXPIRY_MS = 30 * 60 * 1000;
+
+export const PEER_TIMEOUT_MS = 30 * 1000;
+
+export const ROOM_CODE_ALPHABET = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
