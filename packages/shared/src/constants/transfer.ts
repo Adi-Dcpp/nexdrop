987ab5b1aa53aa40a6ff163de020e32236972585
Adi <luffy1120adi@gmail.com>
2026-07-09 14:04:09 +0530

    feat(shared): add transfer protocol constants

---
+++ packages/shared/src/constants/transfer.ts
@@ -0,0 +1,12 @@
+
+export const DEFAULT_CHUNK_SIZE = 64 * 1024;
+
+export const MAX_FILE_SIZE = 20 * 1024 * 1024 * 1024;
+
+export const MAX_CONCURRENT_TRANSFERS = 4;
+
+export const BUFFER_HIGH_WATER_MARK = 8 * 1024 * 1024;
+
+export const BUFFER_LOW_WATER_MARK = 2 * 1024 * 1024;
+
+export const MAX_TRANSFER_RETRIES = 3;
